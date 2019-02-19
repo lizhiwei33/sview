1eeb7fa336e6c19f8cf7878a418e9d812abba59c
lizhiwei33 <lizhiwei_1234@sina.com>
2019-02-19 16:47:59 +0800

    11

--- app/src/main/java/com/lzw/sview/MainActivity.java
+++ app/src/main/java/com/lzw/sview/MainActivity.java
@@ -2,6 +2,7 @@ package com.lzw.sview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"adkfa",Toast.LENGTH_LONG).show();;
     }
 }
